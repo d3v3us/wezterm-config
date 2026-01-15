2f0c44ff43a72f88f7eef31d2074edd740ee3683
Raf Barseghian <42238266+d3v3us@users.noreply.github.com>
2026-01-15 21:04:11 +0000

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,339 +1,210 @@
--- WezTerm Keybindings Documentation by dragonlobster
--- ===================================================
--- Leader Key:
--- The leader key is set to ALT + q, with a timeout of 2000 milliseconds (2 seconds).
--- To execute any keybinding, press the leader key (ALT + q) first, then the corresponding key.
+--- wezterm.lua
+--- $ figlet -f small Wezterm
+--- __      __      _
+--- \ \    / /__ __| |_ ___ _ _ _ __
+---  \ \/\/ / -_)_ /  _/ -_) '_| '  \
+---   \_/\_/\___/__|\__\___|_| |_|_|_|
+---
+--- My Wezterm config file
 
--- Keybindings:
--- 1. Tab Management:
---    - LEADER + c: Create a new tab in the current pane's domain.
---    - LEADER + x: Close the current pane (with confirmation).
---    - LEADER + b: Switch to the previous tab.
---    - LEADER + n: Switch to the next tab.
---    - LEADER + <number>: Switch to a specific tab (0–9).
+local wezterm = require("wezterm")
+local act = wezterm.action
 
--- 2. Pane Splitting:
---    - LEADER + |: Split the current pane horizontally into two panes.
---    - LEADER + -: Split the current pane vertically into two panes.
--- 3. Pane Navigation:
---    - LEADER + h: Move to the pane on the left.
---    - LEADER + j: Move to the pane below.
---    - LEADER + k: Move to the pane above.
---    - LEADER + l: Move to the pane on the right.
+local fish_path = "/opt/homebrew/bin/fish"
 
--- 4. Pane Resizing:
---    - LEADER + LeftArrow: Increase the pane size to the left by 5 units.
---    - LEADER + RightArrow: Increase the pane size to the right by 5 units.
---    - LEADER + DownArrow: Increase the pane size downward by 5 units.
---    - LEADER + UpArrow: Increase the pane size upward by 5 units.
-
--- 5. Status Line:
---    - The status line indicates when the leader key is active, displaying an ocean wave emoji (🌊).
-
--- Miscellaneous Configurations:
--- - Tabs are shown even if there's only one tab.
--- - The tab bar is located at the bottom of the terminal window.
--- - Tab and split indices are zero-based.
-
-
--- Pull in the wezterm API
-local wezterm = require "wezterm"
-
--- This table will hold the configuration.
 local config = {}
+-- Use config builder object if possible
+if wezterm.config_builder then config = wezterm.config_builder() end
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
+-- Settings
+config.default_prog = { fish_path, "-l" }
 
--- max fps
-config.max_fps = 240
-config.animation_fps = 240
+local fav_themes = { "tokyonight", "nordfox" }
+config.color_scheme = fav_themes[2]
+local custom_colors = {
+  red = "#D06F79",     --> Tokyonight: "#F7768e"
+  cyan = "#88C0D0",    --> Tokyonight: "#7DCFFF"
+  magenta = "#B48EAD", --> Tokyonight: "#BB9AF7"
+  yellow = "#EBCB8B",  --> Tokyonight: "#E0AF68"
 
---[[
-============================
-Custom Configuration
-============================
-]] --
+}
 
--- Rounded or Square Style Tabs
-
--- change to square if you don't like rounded tab style
-local tab_style = "square"
-
--- leader active indicator prefix
-local leader_prefix = utf8.char(0x1f30a) -- ocean wave
-
-
---[[
-============================
-Font
-============================
-]] --
-
--- Fixed font to JetBrains Mono, which is widely supported and commonly available
-config.font = wezterm.font_with_fallback { "JetBrains Mono", "FiraCode", "Noto Color Emoji" }
-config.font_size = 14
-
+-- 12 for ComicCode, 16 for official Nerd Fonts
+local font_sizes = { 12.5, 16 }
+config.font_size = font_sizes[1]
+config.font = wezterm.font_with_fallback({
+  { family = "ComicCodeLigatures Nerd Font", },
+  { family = "ProggyClean Nerd Font", scale = 1.2 },
+  { family = "FantasqueSansM Nerd Font", },
+})
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 24
 config.window_decorations = "RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
+config.scrollback_lines = 3000
+config.default_workspace = "main"
 
---[[
-============================
-Colors
-============================
-]] --
-
-local color_scheme = "Catppuccin Macchiato"
-config.color_scheme = color_scheme
-
--- color_scheme not sufficient in providing available colors
--- local colors = wezterm.color.get_builtin_schemes()[color_scheme]
-
--- color scheme colors for easy access
-local scheme_colors = {
-    catppuccin = {
-        macchiato = {
-            rosewater = "f4dbd6",
-            flamingo = "f0c6c6",
-            pink = "f5bde6",
-            mauve = "c6a0f6",
-            red = "ed8796",
-            maroon = "ee99a0",
-            peach = "#f5a97f",
-            yellow = "#eed49f",
-            green = "#a6da95",
-            teal = "#8bd5ca",
-            sky = "#91d7e3",
-            sapphire = "#7dc4e4",
-            blue = "#8aadf4",
-            lavender = "#b7bdf8",
-            text = "#cad3f5",
-            crust = "#181926",
-        }
-    }
+-- Dim inactive panes
+config.inactive_pane_hsb = {
+  saturation = 0.24,
+  brightness = 0.5
 }
 
-local colors = {
-    border = scheme_colors.catppuccin.macchiato.lavender,
-    tab_bar_active_tab_fg = scheme_colors.catppuccin.macchiato.mauve,
-    tab_bar_active_tab_bg = scheme_colors.catppuccin.macchiato.crust,
-    tab_bar_text = scheme_colors.catppuccin.macchiato.crust,
-    arrow_foreground_leader = scheme_colors.catppuccin.macchiato.lavender,
-    arrow_background_leader = scheme_colors.catppuccin.macchiato.crust,
-}
-
-
---[[
-============================
-Border
-============================
-]] --
-
-config.window_frame = {
-    border_left_width = "0.4cell",
-    border_right_width = "0.4cell",
-    border_bottom_height = "0.15cell",
-    border_top_height = "0.15cell",
-    border_left_color = colors.border,
-    border_right_color = colors.border,
-    border_bottom_color = colors.border,
-    border_top_color = colors.border,
-}
-
---[[
-============================
-Shortcuts
-============================
-]] --
-
--- shortcut_configuration
-config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 2000 }
+-- Keys
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-    {
-        mods = "LEADER",
-        key = "c",
-        action = wezterm.action.SpawnTab "CurrentPaneDomain",
-    },
-    {
-        mods = "LEADER",
-        key = "x",
-        action = wezterm.action.CloseCurrentPane { confirm = true }
-    },
-    {
-        mods = "LEADER",
-        key = "b",
-        action = wezterm.action.ActivateTabRelative(-1)
-    },
-    {
-        mods = "LEADER",
-        key = "n",
-        action = wezterm.action.ActivateTabRelative(1)
-    },
-    {
-        mods = "LEADER",
-        key = "|",
-        action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
-    },
-    {
-        mods = "LEADER",
-        key = "-",
-        action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
-    },
-    {
-        mods = "LEADER",
-        key = "h",
-        action = wezterm.action.ActivatePaneDirection "Left"
-    },
-    {
-        mods = "LEADER",
-        key = "j",
-        action = wezterm.action.ActivatePaneDirection "Down"
-    },
-    {
-        mods = "LEADER",
-        key = "k",
-        action = wezterm.action.ActivatePaneDirection "Up"
-    },
-    {
-        mods = "LEADER",
-        key = "l",
-        action = wezterm.action.ActivatePaneDirection "Right"
-    },
-    {
-        mods = "LEADER",
-        key = "LeftArrow",
-        action = wezterm.action.AdjustPaneSize { "Left", 5 }
-    },
-    {
-        mods = "LEADER",
-        key = "RightArrow",
-        action = wezterm.action.AdjustPaneSize { "Right", 5 }
-    },
-    {
-        mods = "LEADER",
-        key = "DownArrow",
-        action = wezterm.action.AdjustPaneSize { "Down", 5 }
-    },
-    {
-        mods = "LEADER",
-        key = "UpArrow",
-        action = wezterm.action.AdjustPaneSize { "Up", 5 }
-    },
+  -- Send C-a when pressing C-a twice
+  { key = "a", mods = "LEADER|CTRL", action = act.SendKey { key = "a", mods = "CTRL" } },
+  { key = "[", mods = "LEADER",      action = act.ActivateCopyMode },
+  { key = ":", mods = "LEADER",      action = act.ActivateCommandPalette },
+
+  -- Workspace (similar to session in Tmux)
+  { key = "s", mods = "LEADER",      action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+
+  -- Tab (similar to window in Tmux)
+  { key = "w", mods = "LEADER",      action = act.ShowTabNavigator },
+  { key = "c", mods = "LEADER",      action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "p", mods = "LEADER",      action = act.ActivateTabRelative(-1) },
+  { key = "n", mods = "LEADER",      action = act.ActivateTabRelative(1) },
+  {
+    key = ",",
+    mods = "LEADER",
+    action = act.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Renaming Tab Title...:" },
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end)
+    }
+  },
+  -- Key table for moving tabs around
+  { key = ".",          mods = "LEADER", action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+  -- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
+  --{ key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
+  --{ key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
+
+  -- Pane
+  { key = "\"",         mods = "LEADER", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+  { key = "%",          mods = "LEADER", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "h",          mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+  { key = "j",          mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+  { key = "k",          mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+  { key = "l",          mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+  { key = "phys:Space", mods = "LEADER", action = act.RotatePanes "Clockwise" },
+  { key = "z",          mods = "LEADER", action = act.TogglePaneZoomState },
+  { key = "x",          mods = "LEADER", action = act.CloseCurrentPane { confirm = true } },
+  {
+    key = '!',
+    mods = 'LEADER | SHIFT',
+    action = wezterm.action_callback(function(win, pane)
+      local tab, window = pane:move_to_new_tab()
+    end),
+  },
+  -- We can make separate keybindings for resizing panes
+  -- But Wezterm offers custom "mode" in the name of "KeyTable"
+  { key = "r", mods = "LEADER", action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
 }
 
-for i = 0, 9 do
-    -- leader + number to activate that tab
-    table.insert(config.keys, {
-        key = tostring(i),
-        mods = "LEADER",
-        action = wezterm.action.ActivateTab(i),
-    })
+-- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = act.ActivateTab(i - 1)
+  })
 end
 
---[[
-============================
-Tab Bar
-============================
-]] --
+config.key_tables = {
+  resize_pane = {
+    { key = "<",      action = act.AdjustPaneSize { "Left", 1 } },
+    { key = "-",      action = act.AdjustPaneSize { "Down", 1 } },
+    { key = "+",      action = act.AdjustPaneSize { "Up", 1 } },
+    { key = ">",      action = act.AdjustPaneSize { "Right", 1 } },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  },
+  move_tab = {
+    { key = "h",      action = act.MoveTabRelative(-1) },
+    { key = "j",      action = act.MoveTabRelative(-1) },
+    { key = "k",      action = act.MoveTabRelative(1) },
+    { key = "l",      action = act.MoveTabRelative(1) },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  }
+}
 
--- tab bar
-config.hide_tab_bar_if_only_one_tab = false
-config.tab_bar_at_bottom = true
+-- Tab bar
+-- I don't like the look of "fancy" tab bar
 config.use_fancy_tab_bar = false
-config.tab_and_split_indices_are_zero_based = true
+config.status_update_interval = 1000
+config.tab_bar_at_bottom = true
+wezterm.on("update-status", function(window, pane)
+  -- Workspace name
+  local stat = window:active_workspace()
+  local stat_color = custom_colors.red
+  -- It's a little silly to have workspace name all the time
+  -- Utilize this to display LDR or current key table name
+  if window:active_key_table() then
+    stat = window:active_key_table()
+    stat_color = custom_colors.cyan
+  end
+  if window:leader_is_active() then
+    stat = "LDR"
+    stat_color = custom_colors.magenta
+  end
 
-local function tab_title(tab_info)
-    local title = tab_info.tab_title
-    -- if the tab title is explicitly set, take that
-    if title and #title > 0 then
-        return title
-    end
-    -- Otherwise, use the title from the active pane
-    -- in that tab
-    return tab_info.active_pane.title
-end
+  local basename = function(s)
+    -- Nothing a little regex can't fix
+    return string.gsub(s, "(.*[/\\])(.*)", "%2")
+  end
 
-wezterm.on(
-    "format-tab-title",
-    function(tab, tabs, panes, config, hover, max_width)
-        local title = " " .. tab.tab_index .. ": " .. tab_title(tab) .. " "
-        local left_edge_text = ""
-        local right_edge_text = ""
+  -- Current working directory
+  local cwd = pane:get_current_working_dir()
+  if cwd then
+    cwd = basename(cwd.file_path) --> URL object introduced in 20240127-113634-bbcac864 (type(cwd) == "userdata")
+    -- cwd = basename(cwd) --> 20230712-072601-f4abf8fd or earlier version
+  else
+    cwd = ""
+  end
 
-        if tab_style == "rounded" then
-            title = tab.tab_index .. ": " .. tab_title(tab)
-            title = wezterm.truncate_right(title, max_width - 2)
-            left_edge_text = wezterm.nerdfonts.ple_left_half_circle_thick
-            right_edge_text = wezterm.nerdfonts.ple_right_half_circle_thick
-        end
+  -- Current command
+  local cmd = pane:get_foreground_process_name()
+  -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
+  cmd = cmd and basename(cmd) or ""
 
-        -- ensure that the titles fit in the available space,
-        -- and that we have room for the edges.
-        -- title = wezterm.truncate_right(title, max_width - 2)
+  -- Left status (left of the tab line)
+  window:set_left_status(wezterm.format({
+    { Foreground = { Color = stat_color } },
+    { Text = "  " },
+    { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+    { Text = " |" },
+  }))
 
-        if tab.is_active then
-            return {
-                { Background = { Color = colors.tab_bar_active_tab_bg } },
-                { Foreground = { Color = colors.tab_bar_active_tab_fg } },
-                { Text = left_edge_text },
-                { Background = { Color = colors.tab_bar_active_tab_fg } },
-                { Foreground = { Color = colors.tab_bar_text } },
-                { Text = title },
-                { Background = { Color = colors.tab_bar_active_tab_bg } },
-                { Foreground = { Color = colors.tab_bar_active_tab_fg } },
-                { Text = right_edge_text },
-            }
-        end
-    end
-)
-
---[[
-============================
-Leader Active Indicator
-============================
-]] --
-
-wezterm.on("update-status", function(window, _)
-    -- leader inactive
-    local solid_left_arrow = ""
-    local arrow_foreground = { Foreground = { Color = colors.arrow_foreground_leader } }
-    local arrow_background = { Background = { Color = colors.arrow_background_leader } }
-    local prefix = ""
-
-    -- leaader is active
-    if window:leader_is_active() then
-        prefix = " " .. leader_prefix
-
-        if tab_style == "rounded" then
-            solid_left_arrow = wezterm.nerdfonts.ple_right_half_circle_thick
-        else
-            solid_left_arrow = wezterm.nerdfonts.pl_left_hard_divider
-        end
-
-        local tabs = window:mux_window():tabs_with_info()
-
-        if tab_style ~= "rounded" then
-            for _, tab_info in ipairs(tabs) do
-                if tab_info.is_active and tab_info.index == 0 then
-                    arrow_background = { Foreground = { Color = colors.tab_bar_active_tab_fg } }
-                    solid_left_arrow = wezterm.nerdfonts.pl_right_hard_divider
-                    break
-                end
-            end
-        end
-    end
-
-
-    window:set_left_status(wezterm.format {
-        { Background = { Color = colors.arrow_foreground_leader } },
-        { Text = prefix },
-        arrow_foreground,
-        arrow_background,
-        { Text = solid_left_arrow }
-    })
+  -- Right status
+  window:set_right_status(wezterm.format({
+    -- Wezterm has a built-in nerd fonts
+    -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
+    { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+    { Text = " | " },
+    { Foreground = { Color = custom_colors.yellow } },
+    { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+    { Text = "  " },
+  }))
 end)
 
--- and finally, return the configuration to wezterm
+--[[ Appearance setting for when I need to take pretty screenshots
+config.enable_tab_bar = false
+config.window_padding = {
+  left = '0.5cell',
+  right = '0.5cell',
+  top = '0.5cell',
+  bottom = '0cell',
+}
+--]]
+
 return config
